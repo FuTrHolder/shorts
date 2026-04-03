a214ede6a482c0389bc474ea09852186d387cfc9
JW_Bear <dmenc.hjw@gmail.com>
2026-04-03 15:48:14 +0900

    Update youtube.py

--- utils/youtube.py
+++ utils/youtube.py
@@ -1,1 +1,24 @@
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google.oauth2.credentials import Credentials
 
+def upload_youtube(video_path, title, description):
+    creds = Credentials.from_authorized_user_file("token.json")
+    youtube = build("youtube", "v3", credentials=creds)
+
+    request = youtube.videos().insert(
+        part="snippet,status",
+        body={
+            "snippet": {
+                "title": title,
+                "description": description
+            },
+            "status": {
+                "privacyStatus": "public"
+            }
+        },
+        media_body=MediaFileUpload(video_path)
+    )
+
+    response = request.execute()
+    return response["id"]
