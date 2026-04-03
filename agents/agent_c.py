3e756ce42e50e9774051827e5baf5b522fbc04a7
JW_Bear <dmenc.hjw@gmail.com>
2026-04-03 15:47:43 +0900

    Update agent_c.py

--- agents/agent_c.py
+++ agents/agent_c.py
@@ -1,6 +1,4 @@
 from utils.youtube import upload_youtube
 
-
-def agent_c(video, title):
-    return upload_youtube(video, title, "자동 생성 영상")
-
+def agent_c(video_path, title):
+    return upload_youtube(video_path, title, "자동 생성 영상")
